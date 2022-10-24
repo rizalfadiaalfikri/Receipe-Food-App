867fc224d298c29ac1f860da111d9570fc53a824
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-10-24 11:17:36 +0700

    Add Fragment

--- app/src/main/java/com/rizalfadiaalfikri/receipefood/Screens/Home/HomeActivity.java
+++ app/src/main/java/com/rizalfadiaalfikri/receipefood/Screens/Home/HomeActivity.java
@@ -1,23 +1,67 @@
 package com.rizalfadiaalfikri.receipefood.Screens.Home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.rizalfadiaalfikri.receipefood.R;
+import com.rizalfadiaalfikri.receipefood.Screens.Fragment.HomeFragment;
+import com.rizalfadiaalfikri.receipefood.Screens.Fragment.MyReceipeFragment;
+import com.rizalfadiaalfikri.receipefood.Screens.Fragment.ProfileFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
         mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
+        replaceFragment(new HomeFragment());
+        bottomNavigation();
+
+    }
+
+    private void bottomNavigation() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        replaceFragment(new HomeFragment());
+                    case R.id.menu_my_receipe:
+                        replaceFragment(new MyReceipeFragment());
+                    case R.id.menu_favorite:
+                        replaceFragment(new HomeFragment());
+                    case R.id.menu_profile:
+                        replaceFragment(new ProfileFragment());
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
